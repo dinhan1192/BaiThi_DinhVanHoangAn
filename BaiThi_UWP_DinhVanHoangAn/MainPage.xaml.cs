78d336b77c023dd3fe188364a689eed4398c5832
dinhan1192 <dinhan1192@gmail.com>
2019-11-06 16:15:23 +0700

    bai thi

--- BaiThi_UWP_DinhVanHoangAn/MainPage.xaml.cs
+++ BaiThi_UWP_DinhVanHoangAn/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BaiThi_UWP_DinhVanHoangAn.Entity;
+using BaiThi_UWP_DinhVanHoangAn.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,9 +25,52 @@ namespace BaiThi_UWP_DinhVanHoangAn
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        static ObservableCollection<Contact> ListAllContact;
+        private ContactModel contactModel;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.contactModel = new ContactModel();
+            this.Loaded += LoadContact;
+        }
+
+        private void LoadContact(object sender, RoutedEventArgs e)
+        {
+            var list = this.contactModel.Select();
+            if (list != null && list.Count != 0)
+            {
+                ListAllContact = new ObservableCollection<Contact>(list);
+                ListViewContact.ItemsSource = ListAllContact;
+            }
+        }
+
+        private void LoadFirstContact(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = Name.Text,
+            };
+
+            var getContact = this.contactModel.SelectFirstContact(contact);
+            if(getContact.PhoneNumber == null)
+            {
+                Warning.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PhoneNumber.Text = getContact.PhoneNumber;
+            }
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            LoadFirstContact(null, null);
+        }
+
+        private void BtnOrCreate_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Pages.InsertContact));
         }
     }
 }
---
+++ BaiThi_UWP_DinhVanHoangAn/Models/ContactModel.cs
@@ -0,0 +1,84 @@
+﻿using BaiThi_UWP_DinhVanHoangAn.Entity;
+using BaiThi_UWP_DinhVanHoangAn.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiThi_UWP_DinhVanHoangAn.Models
+{
+    class ContactModel
+    {
+        public bool Insert(Contact contact)
+        {
+            try
+            {
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare("INSERT INTO Contact (Name, PhoneNumber) VALUES (?,?)"))
+                {
+                    stt.Bind(1, contact.Name);
+                    stt.Bind(2, contact.PhoneNumber);
+                    stt.Step();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+        public Contact SelectFirstContact(Contact contact)
+        {
+            try
+            {
+                var getContact = new Contact();
+                string query = @"SELECT * FROM Contact Where Name = ? ORDER BY ROWID ASC LIMIT 1 ";
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare(query))
+                {
+                    stt.Bind(1, contact.Name);
+                    if (stt.Step() == SQLitePCL.SQLiteResult.ROW)
+                    {
+                        getContact.Id = Convert.ToInt32(stt[0]);
+                        getContact.Name = (string)stt[1];
+                        getContact.PhoneNumber = (string)stt[2];
+                    }
+                    return getContact;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public List<Contact> Select()
+        {
+            try
+            {
+                List<Contact> lstNote = new List<Contact>();
+                string query = @"SELECT * FROM Contact";
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare(query))
+                {
+                    while (stt.Step() == SQLitePCL.SQLiteResult.ROW)
+                    {
+                        var getContact = new Contact();
+                        getContact.Id = Convert.ToInt32(stt[0]);
+                        getContact.Name = (string)stt[1];
+                        getContact.PhoneNumber = (string)stt[2];
+                        lstNote.Add(getContact);
+                    }
+                    return lstNote;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
